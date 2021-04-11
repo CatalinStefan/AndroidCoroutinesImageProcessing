5f37afd96dc248bcca1a85184a7672ad572fb4ec
Marchael Acode <m.acode@outlook.com>
2021-04-11 11:31:21 -0700

    Using coroutines to download an image from the web, apply a filter to transform the image and then apply the filtered image to our UI.

--- app/src/main/java/com/devtides/imageprocessingcoroutines/MainActivity.kt
+++ app/src/main/java/com/devtides/imageprocessingcoroutines/MainActivity.kt
@@ -1,14 +1,55 @@
 package com.devtides.imageprocessingcoroutines
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.devtides.imageprocessingcoroutines.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     private val IMAGE_URL = "https://raw.githubusercontent.com/DevTides/JetpackDogsApp/master/app/src/main/res/drawable/dog.png"
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        coroutineScope.launch {
+            val originalDeferred = coroutineScope.async(Dispatchers.IO) {
+                getOriginalBitmap()
+            }
+
+            val originalBitmap: Bitmap = originalDeferred.await()
+
+            val filteredDeferred = coroutineScope.async(Dispatchers.Default) {
+                applyFilter(originalBitmap)
+            }
+
+            val filterBitmap: Bitmap = filteredDeferred.await()
+
+            loadImage(filterBitmap)
+        }
     }
+
+    private fun getOriginalBitmap() = URL(IMAGE_URL).openStream().use {
+        BitmapFactory.decodeStream(it)
+    }
+
+    private fun applyFilter(originalBitmap: Bitmap) = Filter.apply(originalBitmap)
+
+    private fun loadImage(bitmap: Bitmap) {
+        binding.progressBar.visibility = View.GONE
+        binding.imageView.setImageBitmap(bitmap)
+        binding.imageView.visibility = View.VISIBLE
+    }
+
 }
